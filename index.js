38c03d089bf6da74f1dc8a6e887a4aa37f3de088
radmanlo <radman.lotfiazar@gmail.com>
2023-04-05 12:01:50 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
+
   //add static methods here
+
+  static capitalize (str){
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  static sanitize ( str){
+    return str.replace(/[^- 'A-Za-z]+/g, '')
+  }
+
+  static titleize (str){
+    const temp = str.split(" ")
+    const result = temp.map( (current, index) =>{
+      if (index === 0)
+        return current.charAt(0).toUpperCase() + current.slice(1)
+      if (current !== "a" && current !== 'an' && current !== 'of' && current !== 'and' 
+          && current !== 'for' && current !== 'at' && current !== 'by' && current !== 'from' 
+          && current !== "the" && current !== 'but')
+        return current.charAt(0).toUpperCase() + current.slice(1)
+      return current 
+    })
+    return result.join(" ")
+  }
+
 }
